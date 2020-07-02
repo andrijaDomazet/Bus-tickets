aa19c7605143effdaf3a225927685641416c002a
andrijaDomazet <domazet.andrija@gmail.com>
2020-07-02 09:28:26 +0200

    Working on footer, fix basic issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "./Footer.scss";
+import logo from "../../bus.png";
+import { headerList } from "../../shared/routing";
+import { footerInfo } from "../../shared/footerInfo";
+import { NavLink } from "react-router-dom";
+
 export default function Footer() {
   return (
     <div className="footer">
-      Created by <span>andrijaDomazet</span>
+      <div className="footer__links">
+        <img src={logo} alt="logo" />
+        {headerList.map((item, index) => {
+          return (
+            <NavLink className="footer__link" exact key={index} to={item.route}>
+              {item.title}
+            </NavLink>
+          );
+        })}
+      </div>
+      <div className="footer__info">
+        {footerInfo.map((item, index) => {
+          return (
+            <span key={index}>
+              <i className={`fas fa-${item.icon}`}></i>
+              {item.title}
+            </span>
+          );
+        })}
+      </div>
+      <div>
+        Created by <span>andrijaDomazet</span>
+      </div>
     </div>
   );
 }
